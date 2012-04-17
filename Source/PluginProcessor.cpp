5b53200dfa2ca6c41f73a824c14f0a47fb2194bb
Nik Reiman <nik.reiman@gmail.com>
2012-04-17 09:10:51 +0200

    State starts off as invalid

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -14,6 +14,7 @@
 
 //==============================================================================
 HiLoFilterAudioProcessor::HiLoFilterAudioProcessor() {
+  filterState = kHiLoFilterStateInvalid;
 }
 
 HiLoFilterAudioProcessor::~HiLoFilterAudioProcessor() {
